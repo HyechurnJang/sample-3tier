260f8ede4254b22af82a9a7e011f90f462cfc53a
jzide <jzide@JZ5550>
2022-04-28 17:25:36 +0900

    update

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(20) NOT NULL, email VARCHAR(40) NOT NULL);
+INSERT INTO users(name, email) VALUES('hyechurn', 'jangh@vmkloud.com');
+INSERT INTO users(name, email) VALUES('test1', 'test1@vmkloud.com');
+INSERT INTO users(name, email) VALUES('test2', 'test2@vmkloud.com');
+INSERT INTO users(name, email) VALUES('test3', 'test3@vmkloud.com');
+INSERT INTO users(name, email) VALUES('test4', 'test4@vmkloud.com');
+INSERT INTO users(name, email) VALUES('test5', 'test5@vmkloud.com');
--- db/run.sh
+++ db/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-apt install postgresql-client -y
-docker run --name db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=3tier -p 5432:5432 -d postgres:13.0
+WORKDIR=$(dirname $0)
 
+apt install postgresql-client -y
+docker rm -f db
+docker run --name db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=3tier -p 5432:5432 -d postgres:13.0
+sleep 5
+PGPASSWORD=password psql -h localhost -U postgres -d mydb -f $WORKDIR/init.sql
