dd7009483953a60968d9cdbf2451104740425c77
jzide <jzide@JZ5550>
2022-04-28 18:06:27 +0900

    update

--- env/run.sh
+++ env/run.sh
@@ -16,11 +16,11 @@ WORKDIR=$(dirname $0)
 mkdir -p /opt/registry
 docker rm -f registry
 rm -rf /opt/registry/*
-docker run -d -p 5000:5000 --restart=always --name registry -v /opt/registry:/registry -e "REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/registry" registry:2.7.0
+docker run -d -p 80:5000 --restart=always --name registry -v /opt/registry:/registry -e "REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/registry" registry:2.7.0
 
 cd $WORKDIR/../was
-docker rmi -f $REGISTR:5000Y/was:v1
-docker build --no-cache -t $REGISTRY:5000/was:v1 .
-docker push $REGISTRY:5000/was:v1
+docker rmi -f $REGISTRY/was:v1
+docker build --no-cache -t $REGISTRY/was:v1 .
+docker push $REGISTRY/was:v1
 
 cd $CURRDIR
