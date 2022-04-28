33e3e5dc2fa71342de16dc044673cd7658b0ccef
jzide <jzide@JZ5550>
2022-04-28 17:32:31 +0900

    update

--- db/run.sh
+++ db/run.sh
@@ -5,4 +5,4 @@ WORKDIR=$(dirname $0)
 docker rm -f db
 docker run --name db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=3tier -p 5432:5432 -d postgres:13.0
 sleep 5
-PGPASSWORD=password psql -h localhost -U postgres -d mydb -f $WORKDIR/init.sql
+PGPASSWORD=password psql -h localhost -U postgres -d 3tier -f $WORKDIR/init.sql
