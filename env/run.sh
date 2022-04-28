9391777905a2260bb4120c01d4fa374d1c51f88e
jzide <jzide@JZ5550>
2022-04-28 18:02:03 +0900

    update

--- env/run.sh
+++ env/run.sh
@@ -3,6 +3,13 @@
 # Pre-Register Host: reg3tier
 # Pre-Login Docker to reg3tier
 
+if [ -z "$1" ]; then
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP>"
+    echo ""
+    exit 1
+fi
+REGISTRY=$1
+
 CURRDIR=$(pwd)
 WORKDIR=$(dirname $0)
 
@@ -12,8 +19,8 @@ rm -rf /opt/registry/*
 docker run -d -p 5000:5000 --restart=always --name registry -v /opt/registry:/registry -e "REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/registry" registry:2.7.0
 
 cd $WORKDIR/../was
-docker build --no-cache -t reg3tier/was:v1 .
-docker push reg3tier/was:v1
-
+docker rmi -f $REGISTRY/was:v1
+docker build --no-cache -t $REGISTRY/was:v1 .
+docker push $REGISTRY/was:v1
 
 cd $CURRDIR
