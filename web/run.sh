749758798bdb2111955e61cfa855aa3c69001008
jzide <jzide@JZ5550>
2022-04-28 18:45:21 +0900

    update

--- web/run.sh
+++ web/run.sh
@@ -3,3 +3,21 @@
 # Pre-Register Host: reg3tier
 # Pre-Login Docker to reg3tier
 
+if [ -z "$1" ]; then
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP> <WAS_SERVER_HOST>"
+    echo ""
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP> <WAS_SERVER_HOST>"
+    echo ""
+    exit 1
+fi
+REGISTRY=$1
+WAS_SERVER_HOST=$2
+
+docker rm -f was
+docker rmi -f $REGISTRY/was:v1
+docker run --name web -p 80:80 -e WAS_SERVER_HOST="$WAS_SERVER_HOST" -d vmkloud/web:v1
+sleep 1
+docker logs web
