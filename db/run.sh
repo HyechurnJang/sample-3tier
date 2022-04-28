c5fbcf80214cc0286ae4759bc017e696a685dd0f
jzide <jzide@JZ5550>
2022-04-28 17:28:02 +0900

    update

--- db/run.sh
+++ db/run.sh
@@ -2,7 +2,7 @@
 
 WORKDIR=$(dirname $0)
 
-apt install postgresql-client -y
+apt install docker.io postgresql-client -y
 docker rm -f db
 docker run --name db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=3tier -p 5432:5432 -d postgres:13.0
 sleep 5
