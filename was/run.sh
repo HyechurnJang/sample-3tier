67942bf41226ff36bfd02106fad5326caee22462
jzide <jzide@JZ5550>
2022-04-28 18:31:33 +0900

    update

--- was/run.sh
+++ was/run.sh
@@ -3,5 +3,17 @@
 # Pre-Register Host: reg3tier
 # Pre-Login Docker to reg3tier
 
-docker run --name was -p 8080:8080 -e POSTGRES_HOST="192.168.1.12" -d reg3tier/was:v1
+if [ -z "$1" ]; then
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP> <DATABASE_HOSTNAME_OR_IP>"
+    echo ""
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP> <DATABASE_HOSTNAME_OR_IP>"
+    echo ""
+    exit 1
+fi
+REGISTRY=$1
+DATABASE=$2
 
+docker run --name was -p 8080:8080 -e POSTGRES_HOST="$DATABASE" -d $REGISTRY/was:v1
