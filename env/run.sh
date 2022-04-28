70b475ec91ce307d76f694b6c3cc2198a64ad003
jzide <jzide@JZ5550>
2022-04-28 17:52:16 +0900

    update

--- env/run.sh
+++ env/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Pre-Installed: docker.io
+# Pre-Register Host: reg3tier
+# Pre-Login Docker to reg3tier
+
+CURRDIR=$(pwd)
+WORKDIR=$(dirname $0)
 
 mkdir -p /opt/registry
 docker rm -f registry
 rm -rf /opt/registry/*
 docker run -d -p 5000:5000 --restart=always --name registry -v /opt/registry:/registry -e "REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/registry" registry:2.7.0
+
+cd $WORKDIR/../was
+docker build --no-cache -t reg3tier/was:v1 .
+docker push reg3tier/was:v1
+
+
+cd $CURRDIR
--- was/run.sh
+++ was/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Pre-Installed: docker.io
+# Pre-Register Host: reg3tier
+# Pre-Login Docker to reg3tier
 
+docker run --name was -p 8080:8080 -e POSTGRES_HOST="192.168.1.12" -d reg3tier/was:v1
 
