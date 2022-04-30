6c033d633aa7e91d1efddc565c8d9530f8bf3ccd
jzide <jzide@121.169.135.140>
2022-05-01 02:15:59 +0900

    update

--- env/run.sh
+++ env/run.sh
@@ -4,7 +4,7 @@
 # Pre-Login Docker to reg3tier
 
 if [ -z "$1" ]; then
-    echo "run.sh <REGISTRY_HOSTNAME_OR_IP> <WEB_SERVER_HOST>"
+    echo "run.sh <REGISTRY_HOSTNAME_OR_IP>"
     echo ""
     exit 1
 fi
